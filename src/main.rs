fa86a111a075df949341f9c5b468f8701eaf3de2
samarth <samarthkulkarni@icloud.com>
2025-04-05 17:45:17 +0530

    functionality is solid, server node api left

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,191 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::sync::{mpsc, Arc, Mutex};
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 use std::time::Duration;
 
-// --- Task and Message Types ---
-
 type TaskId = usize;
 
-static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
-
-fn generate_id() -> TaskId {
-    NEXT_ID.fetch_add(1, Ordering::Relaxed)
-}
-
 #[derive(Debug)]
-struct TaskState {
-    variables: Vec<i32>,
-    timer: u64,
+enum ProcessStep {
+    Sleep(u64),
+    Arithmetic,
 }
 
 #[derive(Debug)]
 enum Message {
-    Run,
-    UpdateVar { index: usize, new_value: i32 },
-    Shutdown,
+    CreateTask {
+        id: TaskId,
+        script: String,
+        variables: Vec<i32>,
+        result_tx: mpsc::Sender<(TaskId, Vec<i32>)>,
+    },
+    UpdateTask {
+        id: TaskId,
+        index: usize,
+        new_value: i32,
+    },
+    QueryTask {
+        id: TaskId,
+        response_tx: mpsc::Sender<Vec<i32>>,
+    },
 }
 
 #[derive(Debug)]
-struct WorkerResult {
+enum TaskControl {
+    UpdateVar { index: usize, new_value: i32 },
+    QueryVar { response_tx: mpsc::Sender<Vec<i32>> },
+}
+
+#[derive(Debug)]
+struct Task {
     id: TaskId,
     variables: Vec<i32>,
+    steps: VecDeque<ProcessStep>,
 }
 
+fn parse_script(id: TaskId, script: &str, variables: Vec<i32>) -> Task {
+    let mut steps = VecDeque::new();
+    let mut chars = script.chars().peekable();
 
-struct Hypervisor {
-    registry: Arc<Mutex<HashMap<TaskId, mpsc::Sender<Message>>>>,
-    result_rx: mpsc::Receiver<WorkerResult>,
+    while let Some(ch) = chars.next() {
+        if ch.is_digit(10) {
+            let mut num = ch.to_digit(10).unwrap() as u64;
+            while let Some(next_ch) = chars.peek() {
+                if next_ch.is_digit(10) {
+                    num = num * 10 + next_ch.to_digit(10).unwrap() as u64;
+                    chars.next();
+                } else {
+                    break;
+                }
+            }
+            steps.push_back(ProcessStep::Sleep(num));
+        } else if ch == 'a' {
+            steps.push_back(ProcessStep::Arithmetic);
+        }
+    }
+
+    Task { id, variables, steps }
 }
 
-impl Hypervisor {
-    fn new() -> Self {
-        let (_tx, rx) = mpsc::channel(); // dummy init
-        Self {
-            registry: Arc::new(Mutex::new(HashMap::new())),
-            result_rx: rx,
-        }
-    }
-
-    fn with_result_channel() -> (Self, mpsc::Sender<WorkerResult>) {
-        let (tx, rx) = mpsc::channel();
-        (
-            Self {
-                registry: Arc::new(Mutex::new(HashMap::new())),
-                result_rx: rx,
-            },
-            tx,
-        )
-    }
-
-    fn launch_task(&self, timer: u64, variables: Vec<i32>, result_tx: mpsc::Sender<WorkerResult>) -> TaskId {
-        let (tx, rx) = mpsc::channel();
-        let id = generate_id();
-
-        {
-            let mut reg = self.registry.lock().unwrap();
-            reg.insert(id, tx.clone());
-        }
-
-        let state = TaskState { timer, variables };
-        println!("[Hypervisor] Launching task {id}");
-
-        thread::spawn(move || worker_loop(id, state, rx, result_tx));
-
-        tx.send(Message::Run).unwrap();
-        id
-    }
-
-    fn send_update(&self, task_id: TaskId, index: usize, new_value: i32) {
-        if let Some(sender) = self.registry.lock().unwrap().get(&task_id) {
-            sender.send(Message::UpdateVar { index, new_value }).unwrap();
-            println!("[Hypervisor] Sent update to task {task_id}");
-        }
-    }
-
-    fn listen_for_results(&self) {
-        for result in &self.result_rx {
-            println!("[Hypervisor] Worker {} returned {:?}", result.id, result.variables);
-        }
-    }
-
-    fn shutdown_task(&self, task_id: TaskId) {
-        if let Some(sender) = self.registry.lock().unwrap().remove(&task_id) {
-            let _ = sender.send(Message::Shutdown);
-        }
-    }
-    
-}
-
-fn worker_loop(
-    id: TaskId,
-    mut state: TaskState,
-    rx: mpsc::Receiver<Message>,
-    result_tx: mpsc::Sender<WorkerResult>,
-) {
-    println!("[Worker {id}] Ready with vars = {:?}", state.variables);
-
-    for msg in rx {
-        match msg {
-            Message::Run => {
-                println!("[Worker {id}] Sleeping {}s", state.timer);
-                thread::sleep(Duration::from_secs(state.timer));
-
-                for (i, val) in state.variables.iter_mut().enumerate() {
-                    println!("  [Worker {id}] var[{i}] before = {}", val);
+fn execute_next_step(task: &mut Task) -> bool {
+    if let Some(step) = task.steps.pop_front() {
+        match step {
+            ProcessStep::Sleep(secs) => {
+                println!("[Task {}] Sleeping {}s", task.id, secs);
+                thread::sleep(Duration::from_secs(secs));
+            }
+            ProcessStep::Arithmetic => {
+                for (i, val) in task.variables.iter_mut().enumerate() {
+                    println!("  [Task {}] var[{i}] before = {}", task.id, val);
                     *val += 1;
-                    println!("  [Worker {id}] var[{i}] after = {}", val);
+                    println!("  [Task {}] var[{i}] after = {}", task.id, val);
                 }
-
-                println!("[Worker {id}] Task completed.");
-            }
-
-            Message::UpdateVar { index, new_value } => {
-                if let Some(var) = state.variables.get_mut(index) {
-                    println!("[Worker {id}] Updating var[{index}] to {new_value}");
-                    *var = new_value;
-                }
-            }
-
-            Message::Shutdown => {
-                println!("[Worker {id}] Exiting. Returning results.");
-                let _ = result_tx.send(WorkerResult {
-                    id,
-                    variables: state.variables,
-                });
-                break;
             }
         }
+        true
+    } else {
+        false
     }
+}
+
+fn task_loop(mut task: Task, rx: mpsc::Receiver<TaskControl>, result_tx: mpsc::Sender<(TaskId, Vec<i32>)>) {
+    loop {
+        let has_more = execute_next_step(&mut task);
+
+        while let Ok(msg) = rx.try_recv() {
+            match msg {
+                TaskControl::UpdateVar { index, new_value } => {
+                    if let Some(var) = task.variables.get_mut(index) {
+                        println!("[Task {}] Updating var[{index}] to {}", task.id, new_value);
+                        *var = new_value;
+                    }
+                }
+                TaskControl::QueryVar { response_tx } => {
+                    let _ = response_tx.send(task.variables.clone());
+                }
+            }
+        }
+
+        if !has_more {
+            println!("[Task {}] Finished. Final vars: {:?}", task.id, task.variables);
+            let _ = result_tx.send((task.id, task.variables.clone()));
+            break;
+        }
+    }
+}
+
+
+fn start_worker(rx: mpsc::Receiver<Message>) {
+    let task_map = Arc::new(Mutex::new(HashMap::new()));
+
+    thread::spawn(move || {
+        for msg in rx {
+            match msg {
+                Message::CreateTask { id, script, variables, result_tx } => {
+                    let task = parse_script(id, &script, variables);
+                    let (task_tx, task_rx) = mpsc::channel();
+                    task_map.lock().unwrap().insert(id, task_tx);
+
+                    thread::spawn(move || {
+                        task_loop(task, task_rx, result_tx);
+                    });
+
+                    println!("[Worker] Task {id} created");
+                }
+
+                Message::UpdateTask { id, index, new_value } => {
+                    if let Some(tx) = task_map.lock().unwrap().get(&id) {
+                        tx.send(TaskControl::UpdateVar { index, new_value }).ok();
+                    }
+                }
+
+                Message::QueryTask { id, response_tx } => {
+                    if let Some(tx) = task_map.lock().unwrap().get(&id) {
+                        tx.send(TaskControl::QueryVar { response_tx }).ok();
+                    }
+                }
+            }
+        }
+    });
 }
 
 fn main() {
-    let (hypervisor, result_tx) = Hypervisor::with_result_channel();
+    let (tx, rx) = mpsc::channel();
+    start_worker(rx);
 
-    let task_id = hypervisor.launch_task(3, vec![10, 20], result_tx.clone());
+    let task_id = 1;
+    let (result_tx, result_rx) = mpsc::channel();
+
+    tx.send(Message::CreateTask {
+        id: task_id,
+        script: "2a1a".into(),
+        variables: vec![10, 20],
+        result_tx,
+    }).unwrap();
 
     thread::sleep(Duration::from_secs(1));
-    hypervisor.send_update(task_id, 0, 42);
+    tx.send(Message::UpdateTask {
+        id: task_id,
+        index: 0,
+        new_value: 99,
+    }).unwrap();
 
     thread::sleep(Duration::from_secs(2));
-    hypervisor.shutdown_task(task_id);
 
-    // Drop the last sender — this will close the result channel
-    drop(result_tx);
+    let (resp_tx, resp_rx) = mpsc::channel();
+    tx.send(Message::QueryTask {
+        id: task_id,
+        response_tx: resp_tx,
+    }).unwrap();
 
-    // This thread will exit cleanly after receiving all worker results
-    let result_listener = thread::spawn(move || {
-        hypervisor.listen_for_results();
-    });
+    if let Ok(vars) = resp_rx.recv() {
+        println!("[Server] Query response for Task {task_id}: {:?}", vars);
+    }
 
-    result_listener.join().unwrap();
+    if let Ok((tid, final_vars)) = result_rx.recv() {
+        println!("[Server] Task {tid} completed with final variables: {:?}", final_vars);
+    }
+
+    thread::sleep(Duration::from_secs(1));
 }
-
 
