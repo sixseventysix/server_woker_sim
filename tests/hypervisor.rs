93e0e58c8759e01dd3fb839e87406f3d22c81a53
samarth <samarthkulkarni@icloud.com>
2025-04-06 19:57:20 +0530

    multi query tests

--- tests/hypervisor.rs
+++ tests/hypervisor.rs
@@ -16,7 +16,7 @@ fn test_throttling_rejection() {
         hypervisor.create_task("3", vec![i as i32]);
     }
     hypervisor.create_task("2", vec![99]);
-    thread::sleep(Duration::from_secs(3));
+    // thread::sleep(Duration::from_secs(3));
     hypervisor.listen_for_results();
 }
 
@@ -24,5 +24,17 @@ fn test_throttling_rejection() {
 fn test_invalid_script() {
     let hypervisor = Hypervisor::new();
     hypervisor.create_task("1a!", vec![5, 15]);
+    hypervisor.listen_for_results();
+}
+
+#[test]
+fn test_multiple_queries_and_updates() {
+    let hypervisor = Hypervisor::new();
+    let task_id = 0;
+    hypervisor.create_task("1a1a", vec![10, 20]);
+    hypervisor.update_task(task_id, 0, 99);
+    hypervisor.query_task(task_id, 0);
+    hypervisor.update_task(task_id, 1, 123);
+    hypervisor.query_task(task_id, 1);
     hypervisor.listen_for_results();
 }
